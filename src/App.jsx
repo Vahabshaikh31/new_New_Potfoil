7526ed5d2d23bc5b021a4097094cb5b275a00add
Vahabshaikh31 <svahab33@gmail.com>
2024-04-26 09:40:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import HeroSEction from './components/assets/HeroSEction';
+import './index.css';
+
+function App() {
+  return (
+    <>
+    <div className='main'>
+      <HeroSEction/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/assets/HeroSEction.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+import './HeroSection.css'
+const HeroSEction= () => {
+  return (
+    <div className="Hero">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default HeroSEction
